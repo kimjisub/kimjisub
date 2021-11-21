a2684b86ded30057727a96167088c5c9c7dcca8a
김지섭 <0226daniel@gmail.com>
2021-11-21 11:53:09 +0900

    feat: ratio

--- docs/tools/calc/ratio/func.js
+++ docs/tools/calc/ratio/func.js
@@ -1,79 +1,81 @@
+import { gcd, lcm } from 'mathjs'
+
 function keydown(e, input_num) {
-	if (e.keyCode === 13) check(input_num);
+	if (e.keyCode === 13) check(input_num)
 }
 
 function check(input_num) {
-	let input_a = document.getElementById('a');
-	let input_b = document.getElementById('b');
-	let input_c = document.getElementById('c');
-	let input_d = document.getElementById('d');
-	let integerRatio = document.getElementById('integerRatio');
+	let input_a = document.getElementById('a')
+	let input_b = document.getElementById('b')
+	let input_c = document.getElementById('c')
+	let input_d = document.getElementById('d')
+	let integerRatio = document.getElementById('integerRatio')
 
-	let a = parseFloat(input_a.value);
-	let b = parseFloat(input_b.value);
-	let c = parseFloat(input_c.value);
-	let d = parseFloat(input_d.value);
+	let a = parseFloat(input_a.value)
+	let b = parseFloat(input_b.value)
+	let c = parseFloat(input_c.value)
+	let d = parseFloat(input_d.value)
 
 	let filledNum =
 		(isEmpty(a) ? 0 : 1) +
 		(isEmpty(b) ? 0 : 1) +
 		(isEmpty(c) ? 0 : 1) +
-		(isEmpty(d) ? 0 : 1);
+		(isEmpty(d) ? 0 : 1)
 	if (filledNum >= 3) {
-		if (isEmpty(a)) input_a.value = (b * c) / d;
-		else if (isEmpty(b)) input_b.value = (a * d) / c;
-		else if (isEmpty(c)) input_c.value = (a * d) / b;
-		else if (isEmpty(d)) input_d.value = (b * c) / a;
+		if (isEmpty(a)) input_a.value = (b * c) / d
+		else if (isEmpty(b)) input_b.value = (a * d) / c
+		else if (isEmpty(c)) input_c.value = (a * d) / b
+		else if (isEmpty(d)) input_d.value = (b * c) / a
 		else {
 			switch (input_num) {
 				case 1:
-					input_b.value = (a * d) / c;
-					break;
+					input_b.value = calcRatio(c, d, a)
+					break
 				case 2:
-					input_a.value = (b * c) / d;
-					break;
+					input_a.value = calcRatio(d, c, b)
+					break
 				case 3:
-					input_d.value = (b * c) / a;
-					break;
+					input_d.value = calcRatio(a, b, c)
+					break
 				case 4:
-					input_c.value = (a * d) / b;
-					break;
+					input_c.value = calcRatio(b, a, d)
+					break
 				default:
-					break;
+					break
 			}
 		}
 	}
 
-	let detail = '';
+	let detail = ''
 
 	if (!isEmpty(a) && !isEmpty(b)) {
-		let mul = Math.max(multNumToInteger(a), multNumToInteger(b));
-		a = a * mul;
-		b = b * mul;
-
-		let g = gcd(a, b);
-		detail += `정수 비 ${a / g} : ${b / g}`;
+		const ratio = intRatio(a, b)
+		detail += `정수 비 ${ratio[0]} : ${ratio[1]}`
 	}
 
-	integerRatio.textContent = detail;
+	integerRatio.textContent = detail
+}
+
+function calcRatio(a, b, c) {
+	return (b * c) / a
+}
+
+function intRatio(a, b) {
+	const mul = Math.max(multNumToInteger(a), multNumToInteger(b))
+	const inta = a * mul
+	const intb = b * mul
+
+	const g = gcd(inta, intb)
+	return [inta / g, intb / g]
 }
 function isEmpty(str) {
-	return !str || 0 === str.length;
-}
-
-function gcd(a, b) {
-	while (b !== 0) {
-		let r = a % b;
-		a = b;
-		b = r;
-	}
-	return a;
+	return !str || 0 === str.length
 }
 
 function multNumToInteger(num) {
-	let reg = /\.[0-9]*/g.exec(`${num}`);
-	let point = reg ? reg[0].length - 1 : 0;
-	return Math.pow(10, point);
+	let reg = /\.[0-9]*/g.exec(`${num}`)
+	let point = reg ? reg[0].length - 1 : 0
+	return Math.pow(10, point)
 }
 
-export default { keydown, check, isEmpty, gcd, multNumToInteger };
+export default { keydown, check, isEmpty, gcd, multNumToInteger }
--- docs/tools/calc/ratio/index.js
+++ docs/tools/calc/ratio/index.js
@@ -1,15 +1,67 @@
-import React, { useState } from 'react';
-import func from './index.js';
+import React, { useState } from 'react'
+import func from './func.js'
+import { TextField } from '@mui/material'
+import { gcd, lcm } from 'mathjs'
 
 export default function App() {
+	const [a, setA] = useState(0)
+	const [b, setB] = useState(0)
+	const [c, setC] = useState(0)
+	const [d, setD] = useState(0)
+	const [intRatio, setIntRatio] = useState('')
+
+	//console.log(lcm(1, 1.1))
 	return (
 		<div className="Calculator-Ratio">
+			<TextField
+				id="outlined-number"
+				label="A"
+				type="number"
+				defaultValue={a}
+				InputLabelProps={{
+					shrink: true,
+				}}
+				onKeyDown={(e) => {
+					if (e.code == 'Enter') fun.check(input_num)
+				}}
+			/>
+			:
+			<TextField
+				id="outlined-number"
+				label="B"
+				type="number"
+				defaultValue={b}
+				InputLabelProps={{
+					shrink: true,
+				}}
+			/>
+			=
+			<TextField
+				id="outlined-number"
+				label="C"
+				type="number"
+				defaultValue={c}
+				InputLabelProps={{
+					shrink: true,
+				}}
+			/>
+			:
+			<TextField
+				id="outlined-number"
+				label="D"
+				type="number"
+				defaultValue={d}
+				InputLabelProps={{
+					shrink: true,
+				}}
+			/>
+			<br />
 			<input
 				type="number"
 				step="any"
 				id="a"
 				onKeyDown={(e) => {
-					func.keydown(e, 1);
+					func.keydown(e, 1)
 				}}
 				className="input"
 			/>
@@ -19,7 +71,7 @@ export default function App() {
 				step="any"
 				id="b"
 				onKeyDown={(e) => {
-					func.keydown(e, 2);
+					func.keydown(e, 2)
 				}}
 				className="input"
 			/>
@@ -29,7 +81,7 @@ export default function App() {
 				step="any"
 				id="c"
 				onKeyDown={(e) => {
-					func.keydown(e, 3);
+					func.keydown(e, 3)
 				}}
 				className="input"
 			/>
@@ -39,12 +91,12 @@ export default function App() {
 				step="any"
 				id="d"
 				onKeyDown={(e) => {
-					func.keydown(e, 4);
+					func.keydown(e, 4)
 				}}
 				className="input"
 			/>
 			<br />
 			<p id="integerRatio"></p>
 		</div>
-	);
+	)
 }
