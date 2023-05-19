44134b430a24a39be27a1abe9ddbaebdc8c0fba6
kimjisub <0226daniel@gmail.com>
2023-05-19 12:54:27 +0900

    feat: update resume

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -10,13 +10,18 @@ import ExperienceView from '../../components/resume/ExperienceView';
 
 import ProfileImage from './profile.png';
 
+const Introduce = styled.div``;
+
 export default function Resume() {
   return (
     <Root>
       <Page>
         <Header>
           <Image src={ProfileImage} alt="profile" />
-          <h1>김지섭</h1>
+          <Introduce>
+            <p>asdf</p>
+            <h1>김지섭</h1>
+          </Introduce>
         </Header>
         <section>
           <Gap gap="24px">
@@ -72,14 +77,15 @@ export default function Resume() {
                       [
                         'App',
                         [
-                          'React Native (React Query, Recoil),',
+                          'React Native (TS, Query, Recoil),',
                           'Android Native (Kotlin, Java, Compose),',
                           'Flutter',
                         ],
                       ],
+                      ['Web', 'React.js, Next.js, Wasm (Rust)'],
                       [
                         'Server',
-                        'Node.js, Express.js, Nest.js, Prisma, MySQL, MongoDB, Redis',
+                        'Node.js, Express.js, Nest.js, Hapi, Prisma, MySQL, MongoDB, Redis',
                       ],
                       ['DevOps', 'Docker, AWS, Github Actions'],
                       [
@@ -89,6 +95,7 @@ export default function Resume() {
                           'Design System (Material, Ant, Cupertino)',
                         ],
                       ],
+                      ['Tools', 'Github, Notion, Slack, Linear.app'],
                     ]}
                     colRatio={[1, 4]}
                   />
@@ -104,11 +111,11 @@ export default function Resume() {
               histories={[
                 {
                   title: '학부연구생',
-                  date: '2023.05 - 현재',
+                  date: '2023.04 - 현재',
                   list: [
                     'MySQL TPCC 벤치마크 자동화 도구 개발',
-                    'InnoDB 코드 분석 및 개선',
-                    'OpenSSD를 이용한 NVM 기반 데이터베이스 시스템 개발',
+                    'InnoDB 코드 분석 및 개선점 연구',
+                    'OpenSSD를 이용한 NVM 기반 데이터베이스 시스템 연구',
                   ],
                 },
               ]}
@@ -138,24 +145,85 @@ export default function Resume() {
           <ItemTitle title="프로젝트 및 활동" subtitle="PROJECTS & ACTIVITY" />
           <HistoryBlock
             history={{
-              title: 'UniPad',
+              title: (
+                <A href="https://play.google.com/store/apps/details?id=com.kimjisub.launchpad">
+                  UniPad
+                </A>
+              ),
               date: '2016.02 - 현재',
               list: [
-                '런치패드 기반의 연주형 리듬게임',
-                'Play Store에 출시, 500만+ 다운로드 달성',
+                '런치패드 기반의 연주형 리듬게임 1인 개발',
+                <span key={1}>
+                  <A href="https://play.google.com/store/apps/details?id=com.kimjisub.launchpad">
+                    Play Store
+                  </A>
+                  에 출시, 500만+ 다운로드 달성
+                </span>,
                 '음악게임부문 1위, 전체게임부문 50위 기록',
-                '꾸준한 사용자 커뮤니티 운영',
+                'Task 스케줄링 및 메모이제이션 구현하여 초당 200회 이상의 LED Emulation 처리',
+                'MIDI 장치 연동을 위한 자체 드라이버 인터페이스 개발',
+                <span key={5}>
+                  독자적 디자인 시스템 구축 및 라이브러리{' '}
+                  <A href="https://jitpack.io/#kimjisub/UniPad-DesignKit">
+                    jitpack.io
+                  </A>
+                  에 배포
+                </span>,
+                'AAC Room 이용하여 로컬 데이터베이스 구축',
+                'LiveData, ViewModel, DataBinding 등의 아키텍처 적용',
                 'Java to Kotlin 마이그레이션 진행',
               ],
             }}
           />
           <HistoryBlock
             history={{
-              title: 'Mopl: 아파트 커뮤니티 및 주차 관리 솔루션',
+              title: (
+                <A href="https://play.google.com/store/apps/details?id=com.hanwha.mopl">
+                  Mopl
+                </A>
+              ),
               date: '2022.09 - 현재',
               list: [
                 '한화 비전에서 진행중인 아파트 커뮤니티 및 주차 관리 솔루션 외주 형태 계약',
-                'React Native 기반의 모바일 앱 2개 개발 (입주민용, 관리자용)',
+                '82개의 페이지, 120개의 API를 가진 React Native 기반의 모바일 앱 개발',
+                '36개의 디자인 시스템 컴포넌트 구현',
+                '디자인 팀과 주기적으로 회의 및 피드백 진행',
+                '자체 디자인 규칙 및 컴포넌트 라이브러리 개발',
+              ],
+            }}
+          />
+          <HistoryBlock
+            history={{
+              title: (
+                <A href="https://github.com/kimjisub/video-skipper">
+                  Video Skipper
+                </A>
+              ),
+              date: '2023.04. - 현재',
+              list: [
+                '영상에서 음성이 없는 부분을 자동으로 제거하여 길이를 줄여주는 오픈소스 프로젝트',
+                'Node.js, Typescript, FFmpeg, WASM 등을 활용하여 개발',
+                'Npm.js에 배포, 누적 다운로드 300+ 달성',
+              ],
+            }}
+          />
+          {/* <HistoryBlock
+            history={{
+              title: '행명서',
+              date: '2021.08. - 2022.07',
+              list: [
+                '복잡한 부대 내의 일정을 고려하여 공평한 근무 시간표를 자동으로 생성해주는 웹 서비스',
+                '50여명의 부대원의 근무 시간표를 약 1년간 관리',
+                'Html, CSS, Vanilla JS 를 이용하여 외부 리소스 없이 개발',
+              ],
+            }}
+          /> */}
+          <HistoryBlock
+            history={{
+              title: '설리번 프로젝트 선생님',
+              date: '2019.01.',
+              list: [
+                '코딩 교육 선생님으로서 활동하며 ‘게임으로 배우는 웹해킹’ 커리큘럼 제작 및 교육 진행',
               ],
             }}
           />
@@ -164,42 +232,40 @@ export default function Resume() {
               title: '디미고 라이프',
               date: '2018.03 - 2019.12',
               list: [
-                '한화 비전에서 진행중인 아파트 커뮤니티 및 주차 관리 솔루션 외주 형태 계약',
-                'React Native 기반의 모바일 앱 2개 개발 (입주민용, 관리자용)',
+                '600여명의 한국디지털미디어고등학교 학생들을 위한 전산 서비스 제공',
+                '주말 잔류, 지도일지, 급식 신청 등 학교 생활에 필요한 기능 포함',
+                'Express.js, MySQL, Sequelize 등을 활용하여 REST API 개발',
+                'Socket.io 프로토콜을 활용하여 실시간 현황 모니터링 기능 개발',
+                'Swagger 문서화를 통하여 프론트 팀에게 API 문서 제공',
+                'Excel 불러오기 및 내보내기 기능 개발',
               ],
             }}
           />
           <HistoryBlock
             history={{
-              title: 'Video Skipper',
-              date: '2023.04. - 현재',
+              title: 'CANETIS',
+              date: '2018.03',
               list: [
-                '한화 비전에서 진행중인 아파트 커뮤니티 및 주차 관리 솔루션 외주 형태 계약',
-                'React Native 기반의 모바일 앱 2개 개발 (입주민용, 관리자용)',
+                '시각 장애우를 위한 실내 전용 위치 알림 IoT 서비스',
+                'Firebase의 Firestore, Functions, Messaging 활용하여 서버리스 환경으로 구현',
+                'TTS & STT 기능을 활용하여 음성 기반 인터페이스 설계',
+                'Android Accessibility Service 이용하여 접근성 도구로 앱 제어 기능 제공',
               ],
             }}
           />
           <HistoryBlock
             history={{
-              title: '행명서',
-              date: '2021.08. - 2022.07',
+              title: '카카오톡 채팅 분석기',
+              date: '2015.04.',
               list: [
-                '한화 비전에서 진행중인 아파트 커뮤니티 및 주차 관리 솔루션 외주 형태 계약',
-                'React Native 기반의 모바일 앱 2개 개발 (입주민용, 관리자용)',
+                '카카오톡 대화 내보내기 기능을 이용하여 대화를 분석해주는 앱 개발',
+                '주로 활동한 시간, 가장 많이 사용한 단어 등을 분석해주는 기능 제공',
+                'Java, Android Studio를 활용하여 개발',
+                '5000+ 다운로드 달성',
+                'Material UI 디자인 규칙을 준수하여 개발',
               ],
             }}
           />
-          <HistoryBlock
-            history={{
-              title: '설리번 프로젝트 선생님',
-              date: '2021.08. - 2022.07',
-              list: [
-                '한화 비전에서 진행중인 아파트 커뮤니티 및 주차 관리 솔루션 외주 형태 계약',
-                'React Native 기반의 모바일 앱 2개 개발 (입주민용, 관리자용)',
-              ],
-            }}
-          />
-          교육봉사 대회 수상 프로젝트
         </Article>
       </Page>
     </Root>
@@ -218,8 +284,7 @@ const Root = styled.div`
   flex-direction: column;
   align-items: center;
   color: var(--color-text);
-  transform: scale(1);
-  transform-origin: top left;
+  background-color: #1c1e21;
   @media print {
     align-items: unset;
     box-shadow: none;
@@ -229,6 +294,7 @@ const Root = styled.div`
 
 const Header = styled.header`
   display: flex;
+  margin-bottom: 2em;
 `;
 
 const Article = styled.article``;
@@ -239,6 +305,7 @@ const Image = styled.img`
 
 const A = styled.a`
   color: var(--color-text);
+  text-decoration: underline;
   &:hover {
     text-decoration: underline;
     color: #1c1e21;
