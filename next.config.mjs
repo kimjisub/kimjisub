281723f586fd5ecfb56c5e68738e51c1e3f08261
kimjisub <0226daniel@gmail.com>
2025-06-17 14:57:05 +0900

    ✨ Next.js 설정 및 MDX 지원 추가
    Next.js 설정 파일을 `.mjs`로 변환하고 MDX 지원을 추가함.
    
    - SVG 파일 처리 로직 개선 및 `@svgr/webpack`과 `raw-loader` 활용
    - MDX 파일 확장자 등록 및 `@next/mdx` 사용
    - `package.json`에 MDX 관련 패키지 및 Next.js/React 버전 업데이트 포함
    - 이미지 원격 패턴 설정 유지
    
    향후 MDX 사용 및 SVG 처리 효율성 향상 기대.

---
+++ next.config.mjs
@@ -0,0 +1,67 @@
+import createMDX from '@next/mdx';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'prod-files-secure.s3.us-west-2.amazonaws.com',
+        port: '',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.notion.so',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
+  webpack: config => {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find(rule =>
+      rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      },
+      // Raw loader
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        use: ['raw-loader'],
+        resourceQuery: /raw/, // *.svg?raw
+      },
+    );
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+};
+
+const withMDX = createMDX({
+	extension: /\.(md|mdx)$/,
+});
+
+export default withMDX(nextConfig);
